6448f2d74d55be7c2a34b7e29b91a1ba5ea4c715
Chris Mitchell <chrisjmit@gmail.com>
2017-06-06 17:03:26 +0100

    First test for zombie and jasmine passing

--- spec/features/UserSeesAMapSpec.js
+++ spec/features/UserSeesAMapSpec.js
@@ -1,32 +1,12 @@
-const Browser = require('zombie');
-
-Browser.localhost('example.com', 3000);
+var Browser = require('zombie');
+var url = "http://localhost:3000/";
+var browser = new Browser();
 
 describe('User visits index page', function() {
 
-  const browser = new Browser();
-
-  before(function(done) {
-    browser.visit('/', done);
-  });
-
   it('should see welcome page', function() {
-    browser.assert.text('title', 'Welcome To Brains Depot');
+    browser.visit(url, function(err){
+      expect(browser.success).toBe(true);
+    });
   });
-
-  // describe('see map', function() {
-  //
-  //   before(function(done) {
-  //     browser
-  //       .fill('email',    'zombie@underworld.dead')
-  //       .fill('password', 'eat-the-living')
-  //       .pressButton('Sign Me Up!', done);
-  //   });
-  //
-  //   it('should be successful', function() {
-  //     browser.assert.success();
-  //   });
-  //
-  //
-  // });
 });
