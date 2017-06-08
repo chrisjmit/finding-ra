9a1fbf3199f8a992f089e79b413020f4b48b5a92
Chris Mitchell <chrisjmit@gmail.com>
2017-06-08 12:35:08 +0100

    model added

--- server/models/Space.js
+++
@@ -1,4 +0,0 @@
-function Space() {
-
-}
-module.exports = Space;
--- spec/unit/SpaceSpec.js
+++ spec/unit/SpaceSpec.js
@@ -1,9 +1,16 @@
-var Space = require('../../server/models/Space');
+var Space = require('../../app/models/Space');
 
 describe('Space', function() {
   var space;
 });
 
-beforeEach(function() {
-  space = new Space();
-});
+  beforeEach(function() {
+    space = new Space();
+  });
+
+    it("finds a message", function(){
+      expect(space.hello).toEqual("hello world");
+    });
+
+    it("finds the controller with attributes", function() {
+    });
