c1300cef4c2e06f227c0fd927cf14b735821fa6f
Robert Jones <robert.g.jones@gmail.com>
2017-06-08 14:41:35 +0100

    Scaffold feature test of user clicking map for futher info

---
+++ spec/features/UserClicksMapForMoreInfoSpec.js
@@ -0,0 +1,12 @@
+var Browser = require('zombie');
+var url = "http://localhost:3000/";
+var browser = new Browser();
+
+describe('User clicks space on map', function() {
+  it("shows space specific details", function {
+    browser.visit(url, function(err) {
+      expect()
+    })
+  })
+
+});
